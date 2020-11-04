67d1e5d79931c34cef39787ed51f9f029fd6e354
indonswe <afc2002afc@hotmail.com>
2020-11-04 12:10:01 +0100

    version3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         //1
-        int array[] = new int[] {11, 23, 39};
+        int array[] = new int[] {11, 23, 5};
         //for (int i:array) {
         for (int i=0;i<3;i++){
             System.out.println(array[i]);
@@ -19,14 +19,25 @@ public class App
         //2
         int nr = 5;
         int index = indexOf(array, nr);
+        System.out.println("Index position of number " + nr + " is " + index);
 
 
 
 
     }
 
-    public static int indexOf(int[] array){
-        int index =
+    public static int indexOf(int[] array, int nr){
+        int index = 0;
+        System.out.println(array.length);
+        for (int i=0;i<array.length;i++) {
+                if (array[i]==nr){
+                    index = i;
+                }else{
+                    index = -1;
+                }
+        }
+
+
         return (index);
 
     }
